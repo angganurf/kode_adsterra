cb31646da56a40ff00e4453a4d339c2b33339163
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2024-12-31 06:23:31 +0000

    first commit

---
+++ 728x90.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '1003513031fab7fe79f2e82bc8140c3b',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr'+'ipt type="text/javascript" src="//www.highperformanceformat.com/1003513031fab7fe79f2e82bc8140c3b/invoke.js"></scr'+'ipt>');
