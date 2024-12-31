b4b92f744f63546ef3e81ba45cc698311c7c35d2
Angga Nurf <31128877+angganurf@users.noreply.github.com>
2024-12-31 13:34:50 +0700

    Create 728x90_2.js

---
+++ 728x90_2.js
@@ -0,0 +1,8 @@
+atOptions = {
+    'key' : '1003513031fab7fe79f2e82bc8140c3b',
+    'format' : 'iframe',
+    'height' : 90,
+    'width' : 728,
+    'params' : {}
+};
+document.write('<scr'+'ipt type="text/javascript" src="//cdn.statically.io/gh/angganurf/kode_adsterra/main/invoke.js"></scr'+'ipt>');
